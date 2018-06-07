2605b0cf1d8a003e95cad4bc3a5f0a2dc7d1767f
Haridas Lahari <laharidas@gmail.com>
2018-06-06 20:40:30 -0400

    schema.rb modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_02_195510) do
+ActiveRecord::Schema.define(version: 2018_06_06_231454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2018_06_02_195510) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

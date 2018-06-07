44120a48616482bf26766d4e48238ab755bf009c
Haridas Lahari <laharidas@gmail.com>
2018-06-06 21:20:19 -0400

    Added portfolio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_231454) do
+ActiveRecord::Schema.define(version: 2018_06_07_011214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2018_06_06_231454) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
